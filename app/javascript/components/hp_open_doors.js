88d2590ec9060995cb834c3e3c75b190394b4f3d
renaud dor <renaud.dor@gmail.com>
2020-06-10 11:33:08 +0200

    clean up and comment open door js

--- app/javascript/components/hp_open_doors.js
+++ app/javascript/components/hp_open_doors.js
@@ -1,97 +1,62 @@
 const openDoor = (doorNum) => {
-  const doorAnimation = () => {
+  // define the lines to draw
+  let lineDirection;
+  if (doorNum === 1) {
+    lineDirection = 'left';
+  } else if (doorNum === 2) {
+    lineDirection = 'middle';
+  } else {
+    lineDirection = 'right';
+  }
+
+  const linesToDraw = document.querySelectorAll(`.line-${lineDirection}`);
+
+  // display the three lines of the door to open
+  linesToDraw.forEach((line) => {
+    const lineToDraw = line;
+    lineToDraw.style.display = 'block';
+  });
+
+  // wait for the css animation of the first line to end, then display (open) the door
+  // (the first line as a slightly faster animation thatn the other 2)
+  linesToDraw[0].querySelector('path').addEventListener('animationend', () => {
     const currentDoor = document.querySelector(`.door${doorNum}`);
     currentDoor.style.zIndex = '102';
-  };
+  });
+};
 
-  // recursive function that draw n number of lines one after the other (with n = lineParams.length)
-  // (calling-back itslef in the else statement if there are still lines to draw)
-  // and then call the doorAnimation function
-  const drawLine = async (lineParams) => {
-    const lineLefts = document.querySelectorAll('.line-left');
+// define what pass open what door
+const pass1 = '1214';
+const pass2 = '3324';
+const pass3 = '4123';
 
-    await lineLefts.forEach((line) => {
-      const lineToDraw = line;
-      lineToDraw.style.display = 'block';
-    });
-
-    lineLefts[0].querySelector('path').addEventListener('animationend', () => {
-      doorAnimation();
-    });
-
-    // need to be asynchronus (or as a callback)
-    // so that door animation appear only once lines are drawn
-
-    // const { element, direction, limit } = lineParams[0];
-    // let lineSize = 1;
-    // const lineTimer = setInterval(() => {
-    //   if (lineSize < limit) {
-    //     lineSize += 1;
-    //     element.setAttribute('style', `${direction}: ${lineSize}%`);
-    //   } else {
-    //     clearInterval(lineTimer);
-    //     lineParams.shift();
-    //     if (lineParams.length > 0) {
-    //       drawLine(lineParams);
-    //     } else {
-    //       doorAnimation();
-    //     }
-    //   }
-    // }, 10);
-  };
-
-  // function to help create line objects with an element (html dom element),
-  // a direction (width or height), and a limit (%)
-  const createLine = (element, direction, limit) => ({ element, direction, limit });
-
-  // draw lines corresponding to the door that need to be open
-  if (doorNum === 1) {
-    const line1 = createLine(document.querySelector('.v-line-bottom'), 'height', 100);
-    const line2 = createLine(document.querySelector('.h-line-left'), 'width', 100);
-    const line3 = createLine(document.querySelector('.v-line-left'), 'height', 100);
-    drawLine([line1, line2, line3]);
-  } else if (doorNum === 2) {
-    const line1 = createLine(document.querySelector('.v-line-bottom'), 'height', 100);
-    const line2 = createLine(document.querySelector('.v-line-middle'), 'height', 100);
-    drawLine([line1, line2]);
-  } else {
-    const line1 = createLine(document.querySelector('.v-line-bottom'), 'height', 100);
-    const line2 = createLine(document.querySelector('.h-line-right'), 'width', 100);
-    const line3 = createLine(document.querySelector('.v-line-right'), 'height', 100);
-    drawLine([line1, line2, line3]);
+// check if currentPass equal one of the correct pass
+// if yes, open the corresponding door
+const checkPass = (currentPass) => {
+  switch (currentPass) {
+    case pass1:
+      openDoor(1);
+      break;
+    case pass2:
+      openDoor(2);
+      break;
+    case pass3:
+      openDoor(3);
+      break;
+    default:
+    // no default required, but did it for the linter :)
   }
 };
 
-
+// method that will trigger the opening of the correct door when a password is found
 const hpDoorPassword = () => {
   const bells = document.querySelectorAll('.bell-placeholder');
 
   // array to store the current combination
   const currentPass = [];
 
-  // define what pass open what door
-  const pass1 = '1214';
-  const pass2 = '3324';
-  const pass3 = '4123';
-
-  // check if currentPass equal one of the correct pass
-  const checkPass = (tryPass) => {
-    switch (tryPass) {
-      case pass1:
-        openDoor(1);
-        break;
-      case pass2:
-        openDoor(2);
-        break;
-      case pass3:
-        openDoor(3);
-        break;
-      default:
-      // no default required, but did it for the linter :)
-    }
-  };
-
-  // Each time one bell is click, store its id in the current pass array
+  // Each time one bell is clicked, store its id in the current pass array
+  // and check if current pass is equal to one of the correct pass
   bells.forEach((bell) => {
     bell.addEventListener('click', (event) => {
       // Only store the last clicked bells id
